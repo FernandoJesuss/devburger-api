52d52b54d3338826c67adb217caa4805becc1797
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-01-16 21:43:28 -0300

    Feat: Novas funcionalidades do banco

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,21 @@
 // const { Router } = require("express");
 
 import { Router } from "express";
+import UserController from "./app/controllers/UserController";
 
-import { v4 } from "uuid";
+// import { v4 } from "uuid";
 
-import User from "./app/models/User";
+// import User from "./app/models/User";
 // import { password } from "./config/database";
 
 const routes = new Router();
 
-routes.get("/", async (request, response) => {
-    const user = await User.create({
-        id: v4(),
-        name: "Fernando",
-        email: "fernandomso@email.com",
-        password_hash: "brasil"
-    });
+routes.post("/users", UserController.store);
 
-return response.status(201).json(user);
-    
+
     // return response.status(200).json({message: "Hello World"});
 
-});
+
 
 // module.exports = routes;
 export default routes;
