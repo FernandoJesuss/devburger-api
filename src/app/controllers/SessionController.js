965fc8afd996c7e5aef6f2d2f4ac6dc1012d5191
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-01-21 21:55:36 -0300

    Feat: Criação de tabela products

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,3 +1,57 @@
+// import * as Yup from "yup";
+// import User from "../models/User";
+
+// class SessionController {
+//   async store(request, response) {
+//     const schema = Yup.object({
+//       email: Yup.string().email().required(),
+//       password: Yup.string().min(6).required(),
+//     });
+
+//     const isValid = await schema.isValid(request.body);
+
+//     const emailOrPasswordIncorrect = () =>
+//       response
+//         .status(401)
+//         .json({ error: "Make sure you password are correct" });
+
+//     if (!isValid) {
+//       return emailOrPasswordIncorrect();
+//     }
+
+//     const { email, password } = request.body;
+
+//     const user = await User.findOne({
+//       where: {
+//         email,
+//       },
+//     });
+
+//     if (!user) {
+//       return emailOrPasswordIncorrect();
+//     }
+
+//     const isSamePassword = await user.checkPassword(password);
+
+//     if (!isSamePassword) {
+//       return emailOrPasswordIncorrect();
+//     }
+
+//     return response.status(201).json({
+//       id: user.id,
+//       name: user.name,
+//       email,
+//       admin: user.admin,
+//     });
+//   }
+// }
+
+// export default new SessionController();
+
+
+
+
+
 import * as Yup from "yup";
 import User from "../models/User";
 
@@ -11,39 +65,42 @@ class SessionController {
     const isValid = await schema.isValid(request.body);
 
     const emailOrPasswordIncorrect = () =>
-      response
-        .status(401)
-        .json({ error: "Make sure you password are correct" });
+      response.status(401).json({ error: "E-mail ou senha incorretos." });
 
     if (!isValid) {
-      return emailOrPasswordIncorrect();
+      return response.status(400).json({ error: "Dados inválidos." });
     }
 
     const { email, password } = request.body;
 
-    const user = await User.findOne({
-      where: {
+    try {
+      const user = await User.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (!user) {
+        return emailOrPasswordIncorrect();
+      }
+
+      const isSamePassword = await user.checkPassword(password);
+
+      if (!isSamePassword) {
+        return emailOrPasswordIncorrect();
+      }
+
+      return response.status(200).json({
+        id: user.id,
+        name: user.name,
         email,
-      },
-    });
-
-    if (!user) {
-      return emailOrPasswordIncorrect();
+        admin: user.admin,
+      });
+    } catch (error) {
+      return response.status(500).json({ error: "Erro ao processar a solicitação." });
     }
-
-    const isSamePassword = await user.checkPassword(password);
-
-    if (!isSamePassword) {
-      return emailOrPasswordIncorrect();
-    }
-
-    return response.status(201).json({
-      id: user.id,
-      name: user.name,
-      email,
-      admin: user.admin,
-    });
   }
 }
 
 export default new SessionController();
+
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@
 import { Router } from "express";
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
+import ProductController from "./app/controllers/ProductController";
 
 // import { v4 } from "uuid";
 
@@ -13,6 +14,7 @@ const routes = new Router();
 
 routes.post("/users", UserController.store);
 routes.post("/session", SessionController.store);
+routes.post("/products", ProductController.store);
 
 
     // return response.status(200).json({message: "Hello World"});
