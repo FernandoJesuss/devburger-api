2142a4eeee0c0409cd7d3f31abde4a96ac09ac61
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-01-21 23:12:40 -0300

    Feat: Criação de uploads de img

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 // const { Router } = require("express");
 
 import { Router } from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
+
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 import ProductController from "./app/controllers/ProductController";
@@ -12,9 +15,12 @@ import ProductController from "./app/controllers/ProductController";
 
 const routes = new Router();
 
+const upload = multer(multerConfig);
+
 routes.post("/users", UserController.store);
 routes.post("/session", SessionController.store);
-routes.post("/products", ProductController.store);
+routes.post("/products", upload.single("file"), ProductController.store);
+
 
 
     // return response.status(200).json({message: "Hello World"});
