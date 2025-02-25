d778654172967a4558ac135d22f1ce22dc8faa6d
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-02-25 00:08:48 -0300

    Feat:Novas funcionalidades

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,31 +1,4 @@
 
-// import * as Yup from "yup";
-
-
-// class ProductController {
-//     async store(request, response) {
-//         const schema = Yup.object({
-//             name: Yup.string().required(),
-//             price: Yup.number().required(),
-//             category: Yup.string().required(),
-//         });
-
-//         try {
-//             schema.validateSync(request, body, { abortEarly: false });
-//         } catch (err) {
-//             return response.status(400).json({ error: err.errors });
-//         }
-//         return response.status(201).json({ message: "ok" });
-
-//     }
-// }
-
-// export default new ProductController();
-
-
-
-
-
 import * as Yup from "yup";
 import Product from "../models/Product";
 import Category from "../models/Category";
