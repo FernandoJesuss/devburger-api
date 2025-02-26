77a5782d19bc476cb4c21420cac979c1e499ba56
fernandoJesuss <fernando_ffernando@hotmail.com>
2025-02-25 22:43:51 -0300

    Feat: Criação do containner

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from "sequelize";
+import mongoose from "mongoose";
 
 import configDatabase from "../config/database";
 
@@ -15,6 +16,7 @@ const models = [User, Product, Category];
 class Database {
     constructor() {
         this.init();
+        this.mongo();
 
     }
 
@@ -24,6 +26,12 @@ class Database {
         .map((model) => model.init(this.connection))
         .map(
             (model) => model.associate && model.associate(this.connection.models),
+        );
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect("mongodb://localhost:27017/devburger"
+
         );
     }
 }
